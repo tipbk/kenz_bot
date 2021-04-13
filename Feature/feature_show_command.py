c01b8e2d14ed1e58b906908f761800331c76dbd5
Chanathip Nateprapai <cirzxs@gmail.com>
2021-04-13 16:25:00 +0700

    update command feature

--- Feature/feature_show_command.py
+++ Feature/feature_show_command.py
@@ -1,9 +1,12 @@
 def show_command():
   return '''
       Command List:
+      <Optional> = มีก็ได้ ไม่มีก็ได้
       ? = send a random picture
       hi, hello = KenZ will reply you with random greeting words
       ถาม = He will answer "ไม่รู้"
       roll = KenZ will roll a number from 0 - 100
       xenojam, xeno, server = to check xenojam server status
+      ด่า <Person> = เคนด่า<Person>
+      price <coin_name> = show the current value of <coin_name> if no coin_name he will ask for it
                                     '''
