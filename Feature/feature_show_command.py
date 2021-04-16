d1a2d462df7389b320c2d66d6aa92c6a28037833
Chanathip Nateprapai <cirzxs@gmail.com>
2021-04-16 17:27:50 +0700

    make it more more readable for show command feature

--- Feature/feature_show_command.py
+++ Feature/feature_show_command.py
@@ -1,11 +1,11 @@
 def show_command():
-  return '''
+  return '''```
       Command List:
-      - ? = send a random picture
-      - hi, hello = KenZ will reply you with random greeting words
-      - ถาม = He will answer "ไม่รู้"
-      - roll = KenZ will roll a number from 0 - 100
-      - xenojam, xeno, server = to check xenojam server status
-      - ด่า <Person> = เคนด่า<Person>
-      - coin <coin_name> = show the current value of <coin_name> if no coin_name he will ask for it
+          - ? = send a random picture
+          - hi, hello = KenZ will reply you with random greeting words
+          - ถาม = He will answer "ไม่รู้"
+          - roll = KenZ will roll a number from 0 - 100
+          - xenojam, xeno, server = to check xenojam server status
+          - ด่า <Person> = เคนด่า<Person>
+          - coin <coin_name> = show the current value of <coin_name> if no coin_name he will ask for it```
                                     '''
